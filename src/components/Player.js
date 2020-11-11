ed5fd5eb12f261cd21bc57d49b1660af2b9f1345
Alexander Sarychev <alexander@sarychev.su>
2020-11-11 19:46:08 +0300

    fix: player layout

--- src/components/Player.js
+++ src/components/Player.js
@@ -21,83 +21,127 @@ function Player() {
 
   return (
     <>
-    {size.width>768 &&
-    <div className="header__player player">
-      <audio>
-        <source src="song.mp3" type="audio/mp3" />
-        <p>
-          Your browser doesn't support HTML5 audio. Here is a{" "}
-          <a href="song.mp3">link to the audio</a> instead.
-        </p>
-      </audio>
-      <img
-        className={`player__song-cover ${
-          isOpen ? "player__song-cover_opened" : ""
-        }`}
-        src={song_cover}
-        alt="обложка песни"
-      ></img>
-      <div className={`player__controls-container ${
-              isOpen ? "player__controls-container_opened" : ""
-            }`}>
-        <div className="player__controls">
-          <button
-            className={`player__button player__play button ${
-              isPlaying ? "player__play_active" : ""
+      <div className="header__player player">
+        <audio>
+          <source src="song.mp3" type="audio/mp3" />
+          <p>
+            Your browser doesn't support HTML5 audio. Here is a{" "}
+            <a href="song.mp3">link to the audio</a> instead.
+          </p>
+        </audio>
+        {size.width > 768 && (
+          <img
+            className={`player__song-cover ${
+              isOpen ? "player__song-cover_opened" : ""
             }`}
-            aria-label="play pause"
-            onClick={handlePlayback}
-          />
-          <div className={`player__song-info ${
-            isOpen ? 'player__song-info_opened' : ''}`}>
-            <div className="player__song-container">
-            <span className="player__song_type_curent player__song">
-              Контур — Хадн Дадн feat. Варя Карпова и Федя Быстров{" "}
-            </span>
-            <span className="player__time">2:24</span>
-            </div>
-            <div className="player__timeline">
-              <div className="player__progress" />
-            </div>
-          </div>
-
-          <button className={`player__button player__button_type_text ${
-            isOpen ? "player__button_type_text_opened" : ""}`}>
-            {isOpen ? "Текст песни" : ''}
-          </button>
-          <button
-            className={`player__button player__menu button ${
-              isOpen ? "player__menu_opened" : ""
-            }`}
-            aria-label="menu"
-            onClick={handleMenuClick}
-          />
-        </div>
-
-        <ul
-          className={`player__song-list
-      ${isOpen ? "player__song-list_opened" : ""}`}
+            src={song_cover}
+            alt="обложка песни"
+          ></img>
+        )}
+        <div
+          className={`player__controls-container ${
+            isOpen ? "player__controls-container_opened" : ""
+          }`}
         >
-          <li className="player__song player__song_type_listed">Релизы:</li>
-          <li className="player__song player__song_type_listed">
-            №6 Поезия — Мукулатура feat. Саша Петров
-          </li>
-          <li className="player__song player__song_type_listed">
-            №6 Поезия — Мукулатура feat. Саша Петров
-          </li>
-          <li className="player__song player__song_type_listed">
-            №6 Поезия — Мукулатура feat. Саша Петров
-          </li>
-          <li className="player__song player__song_type_listed">
-            №6 Поезия — Мукулатура feat. Саша Петров
-          </li>
-        </ul>
+          <div className="player__controls">
+            <button
+              className={`player__button player__play button ${
+                isPlaying ? "player__play_active" : ""
+              }`}
+              aria-label="play pause"
+              onClick={handlePlayback}
+            />
+            <div
+              className={`player__song-info ${
+                isOpen ? "player__song-info_opened" : ""
+              }`}
+            >
+              <div className="player__song-container">
+                <span className="player__song_type_curent player__song">
+                  Контур — Хадн Дадн feat. Варя Карпова и Федя Быстров{" "}
+                </span>
+                <span className="player__time">2:24</span>
+              </div>
+              <div className="player__timeline">
+                <div className="player__progress" />
+              </div>
+            </div>
+            <button
+              className={`player__button player__button_type_video ${
+                isOpen ? "player__button_type_video_opened" : ""
+              }`}
+            >
+              {isOpen ? "Клип" : ""}
+            </button>
+            <button
+              className={`player__button player__button_type_text ${
+                isOpen ? "player__button_type_text_opened" : ""
+              }`}
+            >
+              {isOpen ? "Текст песни" : ""}
+            </button>
+            <button
+              className={`player__button player__menu button ${
+                isOpen ? "player__menu_opened" : ""
+              }`}
+              aria-label="menu"
+              onClick={handleMenuClick}
+            />
+          </div>
+          {size.width <= 768 && (
+            <div class="player__playlist-container">
+              <img
+                className={`player__song-cover ${
+                  isOpen ? "player__song-cover_opened" : ""
+                }`}
+                src={song_cover}
+                alt="обложка песни"
+              ></img>
+              <ul
+                className={`player__song-list
+      ${isOpen ? "player__song-list_opened" : ""}`}
+              >
+                <li className="player__song player__song_type_listed">
+                  Релизы:
+                </li>
+                <li className="player__song player__song_type_listed">
+                  №6 Поезия — Мукулатура feat. Саша Петров
+                </li>
+                <li className="player__song player__song_type_listed">
+                  №6 Поезия — Мукулатура feat. Саша Петров
+                </li>
+                <li className="player__song player__song_type_listed">
+                  №6 Поезия — Мукулатура feat. Саша Петров
+                </li>
+                <li className="player__song player__song_type_listed">
+                  №6 Поезия — Мукулатура feat. Саша Петров
+                </li>
+              </ul>
+            </div>
+          )}
+          {size.width > 768 && (
+            <ul
+              className={`player__song-list
+      ${isOpen ? "player__song-list_opened" : ""}`}
+            >
+              <li className="player__song player__song_type_listed">Релизы:</li>
+              <li className="player__song player__song_type_listed">
+                №6 Поезия — Мукулатура feat. Саша Петров
+              </li>
+              <li className="player__song player__song_type_listed">
+                №6 Поезия — Мукулатура feat. Саша Петров
+              </li>
+              <li className="player__song player__song_type_listed">
+                №6 Поезия — Мукулатура feat. Саша Петров
+              </li>
+              <li className="player__song player__song_type_listed">
+                №6 Поезия — Мукулатура feat. Саша Петров
+              </li>
+            </ul>
+          )}
+        </div>
       </div>
-    </div>
-  }
-  {size.width<768 &&
-  <h1>Маленький плеер</h1>
-}
+      
     </>
   );
 }
